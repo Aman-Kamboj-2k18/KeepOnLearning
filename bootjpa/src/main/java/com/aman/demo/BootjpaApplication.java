ecf7eb077e4dd4e76e2aa31a53edb2213d13b5e9
Aman-Kamboj-2k18 <kambojaman1997@gmail.com>
2019-09-07 07:44:30 +0530

    Some random changes

--- bootjpa/src/main/java/com/aman/demo/BootjpaApplication.java
+++ bootjpa/src/main/java/com/aman/demo/BootjpaApplication.java
@@ -1,3 +1,4 @@
+
 package com.aman.demo;
 
 import org.springframework.boot.SpringApplication;
@@ -9,5 +10,4 @@ public class BootjpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BootjpaApplication.class, args);
 	}
-
 }
